4aadf118a1537318b1f6ed6b72145ca4d3b8658e
James Synge <jamessynge@google.com>
2022-03-05 14:34:41 -0500

    Add flash_string_table.*, encapsulates reading the pointers in a flash string table from flash. This should help avoid my earlier mistake of forgetting to use the functions defined in avr/pgmspace.h.

---
+++ extras/tests/flash_string_table_test.cpp
@@ -0,0 +1,89 @@
+#include "flash_string_table.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "progmem_string_data.h"
+
+namespace mcucore {
+namespace test {
+namespace {
+
+MCU_FLASH_STRING_TABLE(table, MCU_FLASHSTR("Entry0"), MCU_FLASHSTR("Entry1"),
+                       MCU_FLASHSTR("Entry2"), );
+
+// Size is implicit (determined via inferred non-type template parameter).
+TEST(FlashStringTableTest, LookupFlashString_ImplicitSize_Int) {
+  EXPECT_EQ(LookupFlashString(table, 0), MCU_FLASHSTR("Entry0"));
+  EXPECT_EQ(LookupFlashString(table, 1), MCU_FLASHSTR("Entry1"));
+  EXPECT_EQ(LookupFlashString(table, 2), MCU_FLASHSTR("Entry2"));
+  EXPECT_EQ(LookupFlashString(table, 3), nullptr);
+}
+
+// Table size is passed explicitly.
+TEST(FlashStringTableTest, LookupFlashString_ExplicitSize_Int) {
+  EXPECT_EQ(LookupFlashString(table, 3, 0), MCU_FLASHSTR("Entry0"));
+  EXPECT_EQ(LookupFlashString(table, 3, 1), MCU_FLASHSTR("Entry1"));
+  EXPECT_EQ(LookupFlashString(table, 3, 2), MCU_FLASHSTR("Entry2"));
+  EXPECT_EQ(LookupFlashString(table, 3, 3), nullptr);
+}
+
+// Zero-based enum.
+enum EnumZero { kZero, kOne, kTwo };
+constexpr auto kBogusThree = static_cast<EnumZero>(3);
+
+TEST(FlashStringTableTest, LookupFlashStringForDenseEnum_MinZero) {
+  EXPECT_EQ(LookupFlashStringForDenseEnum(table, kZero),
+            MCU_FLASHSTR("Entry0"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum(table, kOne), MCU_FLASHSTR("Entry1"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum(table, kTwo), MCU_FLASHSTR("Entry2"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum(table, kBogusThree), nullptr);
+}
+
+// First value above zero.
+enum EnumFive { kFive = 5, kSix, kSeven };
+constexpr auto kBogusFour = static_cast<EnumFive>(4);
+constexpr auto kBogusEight = static_cast<EnumFive>(8);
+
+TEST(FlashStringTableTest, LookupFlashStringForDenseEnum_MinGreaterThanZero) {
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, kFive, kSeven, kFive),
+            MCU_FLASHSTR("Entry0"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, kFive, kSeven, kSix),
+            MCU_FLASHSTR("Entry1"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, kFive, kSeven, kSeven),
+            MCU_FLASHSTR("Entry2"));
+
+  EXPECT_EQ(
+      LookupFlashStringForDenseEnum<int>(table, kFive, kSeven, kBogusFour),
+      nullptr);
+  EXPECT_EQ(
+      LookupFlashStringForDenseEnum<int>(table, kFive, kSeven, kBogusEight),
+      nullptr);
+}
+
+// First value below zero.
+enum class EnumNeg { kB = -1, kC, kD };
+constexpr auto kBogusA = static_cast<EnumNeg>(-2);
+constexpr auto kBogusE = static_cast<EnumNeg>(2);
+
+TEST(FlashStringTableTest, LookupFlashStringForDenseEnum_MinLessThanZero) {
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, EnumNeg::kB, EnumNeg::kD,
+                                               EnumNeg::kB),
+            MCU_FLASHSTR("Entry0"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, EnumNeg::kB, EnumNeg::kD,
+                                               EnumNeg::kC),
+            MCU_FLASHSTR("Entry1"));
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, EnumNeg::kB, EnumNeg::kD,
+                                               EnumNeg::kD),
+            MCU_FLASHSTR("Entry2"));
+
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, EnumNeg::kB, EnumNeg::kD,
+                                               kBogusA),
+            nullptr);
+  EXPECT_EQ(LookupFlashStringForDenseEnum<int>(table, EnumNeg::kB, EnumNeg::kD,
+                                               kBogusE),
+            nullptr);
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace mcucore
---
+++ src/flash_string_table.h
@@ -0,0 +1,81 @@
+#ifndef MCUCORE_SRC_FLASH_STRING_TABLE_H_
+#define MCUCORE_SRC_FLASH_STRING_TABLE_H_
+
+// Support for storing in flash tables of pointers to strings that are
+// themselves stored in flash; i.e. a PROGMEM array of PROGMEM strings.
+//
+// NOTE: So far this only works if the table is stored in the first 64KB of
+// flash because this uses the "near pointer" API.
+
+#include "mcucore_platform.h"
+
+namespace mcucore {
+namespace flash_string_table_internal {
+
+// A pointer to flash stored in flash.
+class FlashStringTableElement {
+ public:
+  constexpr FlashStringTableElement(const __FlashStringHelper* ptr)  // NOLINT
+      : ptr_(ptr) {}
+
+  const __FlashStringHelper* ToFlashStringHelper() const;
+
+ private:
+  const __FlashStringHelper* ptr_;
+};
+
+}  // namespace flash_string_table_internal
+
+using FlashStringTable =
+    const flash_string_table_internal::FlashStringTableElement[];
+
+// Returns the value of the `entry`-th element in the table, which has size
+// `table_size`.
+template <typename T>
+const __FlashStringHelper* LookupFlashString(FlashStringTable table,
+                                             T table_size, T entry) {
+  if (entry < table_size) {
+    return table[entry].ToFlashStringHelper();
+  } else {
+    return nullptr;
+  }
+}
+
+// Returns the value of the `entry`-th element in the table.
+template <typename T, T N>
+const __FlashStringHelper* LookupFlashString(
+    const flash_string_table_internal::FlashStringTableElement (&table)[N],
+    T entry) {
+  return LookupFlashString(table, N, entry);
+}
+
+// Lookup for the case where min_enumerator might be non-zero.
+template <typename U, typename E>
+const __FlashStringHelper* LookupFlashStringForDenseEnum(FlashStringTable table,
+                                                         E min_enumerator,
+                                                         E max_enumerator,
+                                                         E enumerator_value) {
+  if (min_enumerator <= enumerator_value &&
+      enumerator_value <= max_enumerator) {
+    return table[static_cast<U>(enumerator_value) -
+                 static_cast<U>(min_enumerator)]
+        .ToFlashStringHelper();
+  } else {
+    return nullptr;
+  }
+}
+
+template <typename U, typename E, U N>
+const __FlashStringHelper* LookupFlashStringForDenseEnum(
+    const flash_string_table_internal::FlashStringTableElement (&table)[N],
+    E enumerator_value) {
+  return LookupFlashString(table, N, static_cast<U>(enumerator_value));
+}
+
+}  // namespace mcucore
+
+#define MCU_FLASH_STRING_TABLE(table_name, strings...)                  \
+  const ::mcucore::flash_string_table_internal::FlashStringTableElement \
+      table_name[] AVR_PROGMEM = {strings}
+
+#endif  // MCUCORE_SRC_FLASH_STRING_TABLE_H_
